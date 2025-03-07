470d4f9a22a2d237e4565f6d7f5d5d04ea213ffe
Jeff Mbithi <jeffmbithi9@gmail.com>
2025-03-07 10:21:47 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(feet) {
+  if (feet > 0) {
+    return Math.abs(feet - 42) * 264;
+  }
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start * 264 - destination * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = Math.abs((start * 264) - (destination * 264));
+  if (distance <= 400) {
+    return 0;
+  } else if (distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
+
+console.log(calculatesFarePrice(43, 44));
+
